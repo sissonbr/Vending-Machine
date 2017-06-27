033c61ceb949944585088011b8724e221d0028f5
sissonbr <sissonbr@yahoo.com>
2017-06-27 15:10:27 -0400

    Update VendingMachineKata.java
    Converted for unit testing.

--- VendingMachineKata.java
+++ VendingMachineKata.java
@@ -3,17 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class VendingMachineKata {
-	private static double coinTotal = 0;
 	private static String input;
-	private final static double nickel = 0.05;
-	private final static double dime = 0.10;
-	private final static double quarter = 0.25;
-	private final static double colaPrice = 1.00;
-	private final static double chipsPrice = 0.50;
-	private final static double candyPrice = 0.65;
 		
 	public static void main(String[] args) {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		VendingMachine vm = new VendingMachine();
 		
 		while(true)
 		{
@@ -27,82 +21,24 @@ public class VendingMachineKata {
 			
 			switch (input)
 			{
-				case "nickel": acceptCoins(nickel);
+				case "nickel": vm.acceptCoins(vm.nickel);
 				break;
-				case "dime": acceptCoins(dime);
+				case "dime": vm.acceptCoins(vm.dime);
 				break;
-				case "quarter": acceptCoins(quarter);
+				case "quarter": vm.acceptCoins(vm.quarter);
 				break;
-				case "cola": selectProduct("cola");
+				case "cola": vm.selectProduct("cola");
 				break;
-				case "chips": selectProduct("chips");
+				case "chips": vm.selectProduct("chips");
 				break;
-				case "candy": selectProduct("candy");
+				case "candy": vm.selectProduct("candy");
 				break;
-				case "return": returnCoins();
+				case "return": vm.returnCoins();
 				break;
 			}
 		}
 
 	}
 
-	private static void acceptCoins(double coin)
-	{
-		coinTotal += coin;
-	}
-	
-	private static void selectProduct(String product)
-	{
-		switch(product)
-		{
-			case "cola":
-				dispenseProduct(colaPrice, "cola");			
-				break;
-			case "chips":
-				dispenseProduct(chipsPrice, "chips");			
-				break;				
-			case "candy":
-				dispenseProduct(candyPrice, "candy");			
-				break;
-		}
-	}
-	
-	private static void dispenseProduct(double price, String productName)
-	{
-		if (coinTotal >= price)
-		{
-			coinTotal -= price;
-			System.out.println(productName + " dispensed, THANK YOU");
-			makeChange();
-		}	
-		else
-			System.out.println("$"+price);			
-	}
-	
-		private static void makeChange()
-	{
-		if (coinTotal > 0)
-		{
-			System.out.println("Coin return amount: " + coinTotal);
-			coinTotal = 0;
-		}
-	}
-	
-	private static void returnCoins()
-	{
-		System.out.println("Coin return amount: " + coinTotal);
-		coinTotal = 0;
-	}
-	
-	private void soldOut()
-	{
-		System.out.println("SOLD OUT");
-		System.out.println(coinTotal);
-	}
-	
-	private void exactChangeOnly()
-	{
-		System.out.println("EXACT CHANGE ONLY");
-	}
 
 }
